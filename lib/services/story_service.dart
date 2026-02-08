e4ed5fef05772aa2c89bfd1511066c8c7d3d2fe1
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-08 16:38:18 +0500

    Update story_service.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/services/story_service.dart
+++ lib/services/story_service.dart
@@ -2,14 +2,25 @@ import '../core/api.dart';
 import '../core/http_service.dart';
 
 class StoryService {
+  // GET STORIES
   static Future<List<dynamic>> getStories() async {
-    return await HttpService.get(Api.storiesEndpoint);
+    final res = await HttpService.get(Api.storiesEndpoint);
+    return res is List ? res : [];
   }
 
+  // CREATE STORY
+  static Future<void> createStory(String mediaUrl) async {
+    await HttpService.post(
+      Api.storiesEndpoint,
+      {'media_url': mediaUrl},
+    );
+  }
+
+  // MARK AS VIEWED
   static Future<void> markViewed(int storyId) async {
     await HttpService.post(
-      '${Api.storiesEndpoint}/$storyId/view',
-      {},
+      '${Api.storiesEndpoint}/view',
+      {'story_id': storyId},
     );
   }
 }
