9e52ba167858ff0f2160aab268a3f5a693247e70
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-08 16:21:52 +0500

    Update chat_list_screen.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/screens/chat/chat_list_screen.dart
+++ lib/screens/chat/chat_list_screen.dart
@@ -20,66 +20,53 @@ class _ChatListScreenState extends State<ChatListScreen> {
   }
 
   Future<void> _load() async {
-    try {
-      final data = await ChatService.getChats();
-      setState(() {
-        _chats = data;
-        _loading = false;
-      });
-    } catch (_) {
-      setState(() => _loading = false);
-    }
+    final data = await ChatService.getChats();
+    setState(() {
+      _chats = data;
+      _loading = false;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Chats')),
+      appBar: AppBar(
+        title: const Text('Messages'),
+      ),
       body: _loading
           ? const Center(child: CircularProgressIndicator())
-          : RefreshIndicator(
-              onRefresh: _load,
-              child: ListView.builder(
-                itemCount: _chats.length,
-                itemBuilder: (_, i) {
-                  final c = _chats[i];
-                  return ListTile(
-                    leading:
-                        const CircleAvatar(child: Icon(Icons.person)),
-                    title: Text(c['username']),
-                    subtitle: Text(
-                      c['last_message'] ?? '',
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    trailing: c['unread'] > 0
-                        ? CircleAvatar(
-                            radius: 10,
-                            backgroundColor: Colors.green,
-                            child: Text(
-                              '${c['unread']}',
-                              style: const TextStyle(
-                                fontSize: 12,
-                                color: Colors.white,
-                              ),
-                            ),
-                          )
-                        : null,
-                    onTap: () async {
-                      await Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (_) => ChatRoomScreen(
-                            chatId: c['id'],
-                            username: c['username'],
-                          ),
+          : ListView.builder(
+              itemCount: _chats.length,
+              itemBuilder: (_, i) {
+                final c = _chats[i];
+                return ListTile(
+                  leading: const CircleAvatar(child: Icon(Icons.person)),
+                  title: Text(c['username']),
+                  subtitle: Text(
+                    c['last_message'] ?? '',
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  trailing: c['unread'] == true
+                      ? const CircleAvatar(
+                          radius: 6,
+                          backgroundColor: Colors.blue,
+                        )
+                      : null,
+                  onTap: () async {
+                    await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => ChatRoomScreen(
+                          chatId: c['id'],
+                          username: c['username'],
                         ),
-                      );
-                      _load();
-                    },
-                  );
-                },
-              ),
+                      ),
+                    );
+                    _load();
+                  },
+                );
+              },
             ),
     );
   }
