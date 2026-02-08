58acb61f0a11381ff8fb4cc7756d254a5477dcf2
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-08 16:46:01 +0500

    Update follow_service.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/services/follow_service.dart
+++ lib/services/follow_service.dart
@@ -1,30 +1,13 @@
-import '../core/api.dart';
-import '../core/http_service.dart';
+import '../models/user.dart';
 
 class FollowService {
-  static Future<void> followUser(int userId) async {
-    await HttpService.post(
-      '${Api.followEndpoint}/follow',
-      {'user_id': userId},
-    );
-  }
-
-  static Future<void> unfollowUser(int userId) async {
-    await HttpService.post(
-      '${Api.followEndpoint}/unfollow',
-      {'user_id': userId},
-    );
-  }
-
-  static Future<List<dynamic>> getFollowers(String username) async {
-    return await HttpService.get(
-      '${Api.followEndpoint}/$username/followers',
-    );
-  }
-
-  static Future<List<dynamic>> getFollowing(String username) async {
-    return await HttpService.get(
-      '${Api.followEndpoint}/$username/following',
-    );
+  static void toggleFollow(UserModel user) {
+    if (user.isFollowing) {
+      user.isFollowing = false;
+      user.followers--;
+    } else {
+      user.isFollowing = true;
+      user.followers++;
+    }
   }
 }
