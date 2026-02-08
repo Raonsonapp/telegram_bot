9f613b99cda7ccf1d3e8758b94a618fa4f852501
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-08 19:23:44 +0500

    Update story_service.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/services/story_service.dart
+++ lib/services/story_service.dart
@@ -1,26 +1,43 @@
 import '../core/api.dart';
 import '../core/http_service.dart';
+import '../models/story.dart';
 
 class StoryService {
-  // GET STORIES
-  static Future<List<dynamic>> getStories() async {
-    final res = await HttpService.get(Api.storiesEndpoint);
-    return res is List ? res : [];
+  // ================= GET ALL STORIES =================
+  static Future<List<StoryModel>> getStories() async {
+    final response = await HttpService.get(Api.storiesEndpoint);
+
+    return (response as List)
+        .map((json) => StoryModel.fromJson(json))
+        .toList();
   }
 
-  // CREATE STORY
-  static Future<void> createStory(String mediaUrl) async {
-    await HttpService.post(
+  // ================= CREATE STORY =================
+  static Future<StoryModel> createStory({
+    required String mediaUrl,
+  }) async {
+    final response = await HttpService.post(
       Api.storiesEndpoint,
-      {'media_url': mediaUrl},
+      {
+        'media_url': mediaUrl,
+      },
+    );
+
+    return StoryModel.fromJson(response);
+  }
+
+  // ================= MARK STORY AS VIEWED =================
+  static Future<void> markViewed(int storyId) async {
+    await HttpService.post(
+      '${Api.storiesEndpoint}/$storyId/view',
+      {},
     );
   }
 
-  // MARK AS VIEWED
-  static Future<void> markViewed(int storyId) async {
-    await HttpService.post(
-      '${Api.storiesEndpoint}/view',
-      {'story_id': storyId},
+  // ================= DELETE STORY =================
+  static Future<void> deleteStory(int storyId) async {
+    await HttpService.delete(
+      '${Api.storiesEndpoint}/$storyId',
     );
   }
 }
