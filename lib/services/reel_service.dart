93ae4eb89b25206a8c469ef71cab0554a73362f3
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-08 20:12:40 +0500

    Update reel_service.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/services/reel_service.dart
+++ lib/services/reel_service.dart
@@ -1,26 +1,62 @@
 import '../core/api.dart';
 import '../core/http_service.dart';
+import '../models/reel.dart';
 
 class ReelService {
-  // GET REELS
-  static Future<List<dynamic>> getReels() async {
-    final res = await HttpService.get(Api.reelsEndpoint);
-    return res is List ? res : [];
+  // ================= GET REELS FEED =================
+  static Future<List<ReelModel>> getReels() async {
+    final response = await HttpService.get(Api.reelsEndpoint);
+
+    return (response as List)
+        .map((json) => ReelModel.fromJson(json))
+        .toList();
   }
 
-  // LIKE
-  static Future<void> like(int reelId) async {
+  // ================= CREATE REEL =================
+  static Future<ReelModel> createReel({
+    required String videoUrl,
+    String? caption,
+  }) async {
+    final response = await HttpService.post(
+      Api.reelsEndpoint,
+      {
+        'video_url': videoUrl,
+        'caption': caption,
+      },
+    );
+
+    return ReelModel.fromJson(response);
+  }
+
+  // ================= LIKE REEL =================
+  static Future<void> likeReel(int reelId) async {
     await HttpService.post(
-      '${Api.reelsEndpoint}/like',
-      {'reel_id': reelId},
+      '${Api.reelsEndpoint}/$reelId/like',
+      {},
     );
   }
 
-  // UNLIKE
-  static Future<void> unlike(int reelId) async {
+  // ================= UNLIKE REEL =================
+  static Future<void> unlikeReel(int reelId) async {
     await HttpService.post(
-      '${Api.reelsEndpoint}/unlike',
-      {'reel_id': reelId},
+      '${Api.reelsEndpoint}/$reelId/unlike',
+      {},
+    );
+  }
+
+  // ================= SAVE REEL =================
+  static Future<void> saveReel(int reelId) async {
+    await HttpService.post(
+      '${Api.reelsEndpoint}/$reelId/save',
+      {},
+    );
+  }
+
+  // ================= UNSAVE REEL =================
+  static Future<void> unsaveReel(int reelId) async {
+    await HttpService.post(
+      '${Api.reelsEndpoint}/$reelId/unsave',
+      {},
     );
   }
 }
