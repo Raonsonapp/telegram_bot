236a1c401aaffde734f9aae9a0348ff14ec45aa7
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-08 16:35:40 +0500

    Update post_service.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/services/post_service.dart
+++ lib/services/post_service.dart
@@ -1,140 +1,57 @@
 import '../core/api.dart';
 import '../core/http_service.dart';
-import '../models/post.dart';
-import '../models/story.dart';
 
 class PostService {
-  // ===================== FEED =====================
+  // FEED
   static Future<List<dynamic>> getFeedPosts() async {
     final res = await HttpService.get(Api.postsEndpoint);
-    return res as List<dynamic>;
+    return res is List ? res : [];
   }
 
-  static Future<List<dynamic>> getReels() async {
-    final res = await HttpService.get(Api.reelsEndpoint);
-    return res as List<dynamic>;
-  }
-
-  static Future<List<dynamic>> getByUser(String username) async {
-    final res = await HttpService.get(
-      '${Api.postsEndpoint}/user/$username',
-    );
-    return res as List<dynamic>;
-  }
-
-  // ===================== CREATE =====================
+  // CREATE POST
   static Future<void> createPost({
-    required String mediaUrl,
     required String caption,
+    required String mediaUrl,
   }) async {
     await HttpService.post(
       Api.postsEndpoint,
       {
-        'media_url': mediaUrl,
         'caption': caption,
+        'media_url': mediaUrl,
       },
     );
   }
 
-  static Future<void> createReel({
-    required String mediaUrl,
-    required String caption,
-  }) async {
-    await HttpService.post(
-      Api.reelsEndpoint,
-      {
-        'media_url': mediaUrl,
-        'caption': caption,
-      },
-    );
-  }
-
-  static Future<void> deletePost(int postId) async {
-    await HttpService.delete(
-      '${Api.postsEndpoint}/$postId',
-    );
-  }
-
-  // ===================== LIKES =====================
+  // LIKE / UNLIKE
   static Future<void> likePost(int postId) async {
     await HttpService.post(
-      '${Api.postsEndpoint}/$postId/like',
-      {},
+      '${Api.postsEndpoint}/like',
+      {'post_id': postId},
     );
   }
 
   static Future<void> unlikePost(int postId) async {
     await HttpService.post(
-      '${Api.postsEndpoint}/$postId/unlike',
-      {},
+      '${Api.postsEndpoint}/unlike',
+      {'post_id': postId},
     );
   }
 
-  // ===================== COMMENTS =====================
+  // COMMENTS
   static Future<List<dynamic>> getComments(int postId) async {
     final res = await HttpService.get(
       '${Api.postsEndpoint}/$postId/comments',
     );
-    return res as List<dynamic>;
+    return res is List ? res : [];
   }
 
-  static Future<void> addComment({
-    required int postId,
-    required String text,
-  }) async {
+  static Future<void> addComment(int postId, String text) async {
     await HttpService.post(
-      '${Api.postsEndpoint}/$postId/comments',
-      {'text': text},
+      '${Api.postsEndpoint}/comment',
+      {
+        'post_id': postId,
+        'text': text,
+      },
     );
   }
-
-  // ===================== SAVED =====================
-  static Future<void> savePost(int postId) async {
-    await HttpService.post(
-      '${Api.postsEndpoint}/$postId/save',
-      {},
-    );
-  }
-
-  static Future<void> unsavePost(int postId) async {
-    await HttpService.post(
-      '${Api.postsEndpoint}/$postId/unsave',
-      {},
-    );
-  }
-
-  static Future<List<dynamic>> getSavedPosts() async {
-    final res = await HttpService.get(
-      '${Api.postsEndpoint}/saved',
-    );
-    return res as List<dynamic>;
-  }
-
-  // ===================== STORIES =====================
-  static Future<List<dynamic>> getStories() async {
-    final res = await HttpService.get(Api.storiesEndpoint);
-    return res as List<dynamic>;
-  }
-
-  static Future<void> createStory({
-    required String mediaUrl,
-  }) async {
-    await HttpService.post(
-      Api.storiesEndpoint,
-      {'media_url': mediaUrl},
-    );
-  }
-
-  static Future<void> markStoryViewed(int storyId) async {
-    await HttpService.post(
-      '${Api.storiesEndpoint}/$storyId/view',
-      {},
-    );
-  }
-  // илова ба файл
-static Future<List<dynamic>> getByUser(String username) async {
-  final res =
-      await HttpService.get('${Api.postsEndpoint}/user/$username');
-  return List<dynamic>.from(res);
-}
 }
