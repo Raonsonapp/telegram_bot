9b0febaf7cac2ede1126b86a0879fc6fcab1cb04
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-08 20:15:52 +0500

    Update follow_service.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/services/follow_service.dart
+++ lib/services/follow_service.dart
@@ -1,13 +1,52 @@
+import '../core/api.dart';
+import '../core/http_service.dart';
 import '../models/user.dart';
 
 class FollowService {
-  static void toggleFollow(UserModel user) {
-    if (user.isFollowing) {
-      user.isFollowing = false;
-      user.followers--;
-    } else {
-      user.isFollowing = true;
-      user.followers++;
-    }
+  // ================= FOLLOW USER =================
+  static Future<void> followUser(int userId) async {
+    await HttpService.post(
+      '${Api.followEndpoint}/$userId/follow',
+      {},
+    );
+  }
+
+  // ================= UNFOLLOW USER =================
+  static Future<void> unfollowUser(int userId) async {
+    await HttpService.post(
+      '${Api.followEndpoint}/$userId/unfollow',
+      {},
+    );
+  }
+
+  // ================= GET FOLLOWERS =================
+  static Future<List<UserModel>> getFollowers(int userId) async {
+    final response = await HttpService.get(
+      '${Api.followEndpoint}/$userId/followers',
+    );
+
+    return (response as List)
+        .map((json) => UserModel.fromJson(json))
+        .toList();
+  }
+
+  // ================= GET FOLLOWING =================
+  static Future<List<UserModel>> getFollowing(int userId) async {
+    final response = await HttpService.get(
+      '${Api.followEndpoint}/$userId/following',
+    );
+
+    return (response as List)
+        .map((json) => UserModel.fromJson(json))
+        .toList();
+  }
+
+  // ================= CHECK IS FOLLOWING =================
+  static Future<bool> isFollowing(int userId) async {
+    final response = await HttpService.get(
+      '${Api.followEndpoint}/$userId/is-following',
+    );
+
+    return response['following'] == true;
   }
 }
