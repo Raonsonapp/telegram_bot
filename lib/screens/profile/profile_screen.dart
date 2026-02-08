cadc61bacd4d2221cb41bbdd93ffea9f442b5c30
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-08 16:24:52 +0500

    Update profile_screen.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-import '../../core/session.dart';
 import '../../services/follow_service.dart';
 import '../../services/post_service.dart';
+import 'follow_list_screen.dart';
 
 class ProfileScreen extends StatefulWidget {
-  final String? username; // null => my profile
-  const ProfileScreen({super.key, this.username});
+  final String username;
+
+  const ProfileScreen({super.key, required this.username});
 
   @override
   State<ProfileScreen> createState() => _ProfileScreenState();
@@ -13,58 +14,24 @@ class ProfileScreen extends StatefulWidget {
 
 class _ProfileScreenState extends State<ProfileScreen> {
   bool _loading = true;
-  bool _following = false;
-
-  String _me = '';
-  String _user = '';
-  String _bio = '';
-  bool _verified = false;
-
-  int _postsCount = 0;
-  int _followers = 0;
-  int _followingCount = 0;
-
+  Map<String, dynamic>? _user;
   List<dynamic> _posts = [];
 
   @override
   void initState() {
     super.initState();
-    _init();
+    _load();
   }
 
-  Future<void> _init() async {
-    _me = await Session.username() ?? '';
-    _user = widget.username ?? _me;
-    await _loadProfile();
-  }
+  Future<void> _load() async {
+    final user = await PostService.getUser(widget.username);
+    final posts = await PostService.getByUser(widget.username);
 
-  Future<void> _loadProfile() async {
-    try {
-      final counts = await FollowService.getCounts(_user);
-      final posts = await PostService.getByUser(_user);
-
-      setState(() {
-        _bio = counts['bio'] ?? '';
-        _verified = counts['verified'] == true;
-        _followers = counts['followers'];
-        _followingCount = counts['following'];
-        _following = counts['is_following'] == true;
-        _posts = posts;
-        _postsCount = posts.length;
-        _loading = false;
-      });
-    } catch (_) {
-      setState(() => _loading = false);
-    }
-  }
-
-  Future<void> _toggleFollow() async {
-    if (_following) {
-      await FollowService.unfollow(_user);
-    } else {
-      await FollowService.follow(_user);
-    }
-    _loadProfile();
+    setState(() {
+      _user = user;
+      _posts = posts;
+      _loading = false;
+    });
   }
 
   @override
@@ -79,8 +46,8 @@ class _ProfileScreenState extends State<ProfileScreen> {
       appBar: AppBar(
         title: Row(
           children: [
-            Text(_user),
-            if (_verified)
+            Text(_user!['username']),
+            if (_user!['verified'] == true)
               const Padding(
                 padding: EdgeInsets.only(left: 6),
                 child: Icon(Icons.verified, color: Colors.green, size: 18),
@@ -91,7 +58,6 @@ class _ProfileScreenState extends State<ProfileScreen> {
       body: ListView(
         children: [
           _header(),
-          _bioSection(),
           const Divider(),
           _grid(),
         ],
@@ -99,96 +65,106 @@ class _ProfileScreenState extends State<ProfileScreen> {
     );
   }
 
-  // ===== HEADER =====
   Widget _header() {
     return Padding(
       padding: const EdgeInsets.all(16),
-      child: Row(
-        children: [
-          const CircleAvatar(
-            radius: 36,
-            child: Icon(Icons.person, size: 36),
-          ),
-          Expanded(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                _stat('Posts', _postsCount),
-                _stat('Followers', _followers),
-                _stat('Following', _followingCount),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _stat(String label, int value) {
-    return Column(
-      children: [
-        Text('$value',
-            style: const TextStyle(fontWeight: FontWeight.bold)),
-        Text(label),
-      ],
-    );
-  }
-
-  // ===== BIO + FOLLOW =====
-  Widget _bioSection() {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          if (_bio.isNotEmpty)
-            Padding(
-              padding: const EdgeInsets.only(bottom: 8),
-              child: Text(_bio),
-            ),
-          if (_user != _me)
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor:
-                      _following ? Colors.grey : Colors.green,
-                ),
-                onPressed: _toggleFollow,
-                child: Text(_following ? 'Following' : 'Follow'),
+          const CircleAvatar(radius: 40, child: Icon(Icons.person, size: 40)),
+          const SizedBox(height: 8),
+          Text(_user!['bio'] ?? ''),
+          const SizedBox(height: 12),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _count('Posts', _posts.length),
+              _count(
+                'Followers',
+                _user!['followers'],
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => FollowListScreen(
+                        username: widget.username,
+                        isFollowers: true,
+                      ),
+                    ),
+                  );
+                },
               ),
-            ),
+              _count(
+                'Following',
+                _user!['following'],
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => FollowListScreen(
+                        username: widget.username,
+                        isFollowers: false,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+          const SizedBox(height: 12),
+          _followButton(),
         ],
       ),
     );
   }
 
-  // ===== GRID =====
-  Widget _grid() {
-    if (_posts.isEmpty) {
-      return const Padding(
-        padding: EdgeInsets.all(40),
-        child: Center(child: Text('No posts yet')),
-      );
-    }
+  Widget _followButton() {
+    final bool isFollowing = _user!['is_following'] == true;
 
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: () async {
+          if (isFollowing) {
+            await FollowService.unfollowUser(_user!['id']);
+          } else {
+            await FollowService.followUser(_user!['id']);
+          }
+          _load();
+        },
+        style: ElevatedButton.styleFrom(
+          backgroundColor: isFollowing ? Colors.grey[800] : Colors.blue,
+        ),
+        child: Text(isFollowing ? 'Unfollow' : 'Follow'),
+      ),
+    );
+  }
+
+  Widget _count(String label, int value, {VoidCallback? onTap}) {
+    return InkWell(
+      onTap: onTap,
+      child: Column(
+        children: [
+          Text(
+            '$value',
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+          Text(label),
+        ],
+      ),
+    );
+  }
+
+  Widget _grid() {
     return GridView.builder(
       shrinkWrap: true,
       physics: const NeverScrollableScrollPhysics(),
-      gridDelegate:
-          const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 3,
-        crossAxisSpacing: 2,
-        mainAxisSpacing: 2,
-      ),
       itemCount: _posts.length,
+      gridDelegate:
+          const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
       itemBuilder: (_, i) {
-        final p = _posts[i];
         return Image.network(
-          p['image_url'],
+          _posts[i]['image_url'],
           fit: BoxFit.cover,
-          errorBuilder: (_, __, ___) =>
-              const Icon(Icons.broken_image),
         );
       },
     );
