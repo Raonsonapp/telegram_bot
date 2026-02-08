976a96ae3dbceefa57e1d494b2c23ad947b607da
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-08 19:17:06 +0500

    Update user.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,21 +1,72 @@
 class UserModel {
-  final int id;
+  final String id;
   final String username;
-  final String avatar;
-  final String bio;
-  int followers;
-  int following;
-  bool isVerified;
-  bool isFollowing;
+  final String? email;
+  final String? avatarUrl;
+  final String? bio;
+  final int followersCount;
+  final int followingCount;
+  final bool isVerified;
 
   UserModel({
     required this.id,
     required this.username,
-    required this.avatar,
-    required this.bio,
-    this.followers = 0,
-    this.following = 0,
-    this.isVerified = false,
-    this.isFollowing = false,
+    this.email,
+    this.avatarUrl,
+    this.bio,
+    required this.followersCount,
+    required this.followingCount,
+    required this.isVerified,
   });
+
+  // ================= FROM JSON =================
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      id: json['id'].toString(),
+      username: json['username'] ?? '',
+      email: json['email'],
+      avatarUrl: json['avatar'],
+      bio: json['bio'],
+      followersCount: json['followers_count'] ?? 0,
+      followingCount: json['following_count'] ?? 0,
+      isVerified: json['is_verified'] ?? false,
+    );
+  }
+
+  // ================= TO JSON =================
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'username': username,
+      'email': email,
+      'avatar': avatarUrl,
+      'bio': bio,
+      'followers_count': followersCount,
+      'following_count': followingCount,
+      'is_verified': isVerified,
+    };
+  }
+
+  // ================= COPY WITH =================
+  UserModel copyWith({
+    String? id,
+    String? username,
+    String? email,
+    String? avatarUrl,
+    String? bio,
+    int? followersCount,
+    int? followingCount,
+    bool? isVerified,
+  }) {
+    return UserModel(
+      id: id ?? this.id,
+      username: username ?? this.username,
+      email: email ?? this.email,
+      avatarUrl: avatarUrl ?? this.avatarUrl,
+      bio: bio ?? this.bio,
+      followersCount: followersCount ?? this.followersCount,
+      followingCount: followingCount ?? this.followingCount,
+      isVerified: isVerified ?? this.isVerified,
+    );
+  }
 }
