7eed654b20bccfc8c42d34cec024abc78abfbea2
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-08 16:59:06 +0500

    Update bottom_nav.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/navigation/bottom_nav.dart
+++ lib/navigation/bottom_nav.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import '../screens/home/feed_screen.dart';
+
+import '../screens/home/home_screen.dart';
 import '../screens/search/search_screen.dart';
 import '../screens/reels/reels_screen.dart';
 import '../screens/chat/chat_list_screen.dart';
@@ -16,7 +17,7 @@ class _BottomNavState extends State<BottomNav> {
   int _index = 0;
 
   final List<Widget> _screens = const [
-    FeedScreen(),
+    HomeScreen(),
     SearchScreen(),
     ReelsScreen(),
     ChatListScreen(),
@@ -26,19 +27,23 @@ class _BottomNavState extends State<BottomNav> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _screens[_index],
+      body: IndexedStack(
+        index: _index,
+        children: _screens,
+      ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _index,
         onTap: (i) => setState(() => _index = i),
         type: BottomNavigationBarType.fixed,
-        backgroundColor: Colors.black,
+        backgroundColor: const Color(0xFF0F1424),
         selectedItemColor: Colors.white,
-        unselectedItemColor: Colors.grey,
+        unselectedItemColor: Colors.white54,
         showSelectedLabels: false,
         showUnselectedLabels: false,
         items: const [
           BottomNavigationBarItem(
-            icon: Icon(Icons.home_filled),
+            icon: Icon(Icons.home_outlined),
+            activeIcon: Icon(Icons.home),
             label: 'Home',
           ),
           BottomNavigationBarItem(
@@ -47,14 +52,17 @@ class _BottomNavState extends State<BottomNav> {
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.movie_outlined),
+            activeIcon: Icon(Icons.movie),
             label: 'Reels',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.chat_bubble_outline),
+            activeIcon: Icon(Icons.chat_bubble),
             label: 'Chat',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.person_outline),
+            activeIcon: Icon(Icons.person),
             label: 'Profile',
           ),
         ],
