5091e1584111f4cee86660ec9540c3855cf16e7f
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-08 16:38:46 +0500

    Update story_bar.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/screens/home/story_bar.dart
+++ lib/screens/home/story_bar.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../services/story_service.dart';
-import '../stories/story_view_screen.dart';
+import '../story/story_view_screen.dart';
 
 class StoryBar extends StatefulWidget {
   const StoryBar({super.key});
@@ -11,6 +11,7 @@ class StoryBar extends StatefulWidget {
 
 class _StoryBarState extends State<StoryBar> {
   List<dynamic> _stories = [];
+  bool _loading = true;
 
   @override
   void initState() {
@@ -20,46 +21,64 @@ class _StoryBarState extends State<StoryBar> {
 
   Future<void> _load() async {
     final data = await StoryService.getStories();
-    setState(() => _stories = data);
+    setState(() {
+      _stories = data;
+      _loading = false;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    if (_stories.isEmpty) return const SizedBox();
+    if (_loading) {
+      return const SizedBox(
+        height: 90,
+        child: Center(child: CircularProgressIndicator()),
+      );
+    }
 
     return SizedBox(
-      height: 95,
+      height: 100,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
         itemCount: _stories.length,
         itemBuilder: (_, i) {
           final s = _stories[i];
           return GestureDetector(
-            onTap: () {
-              Navigator.push(
+            onTap: () async {
+              await Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (_) => StoryViewScreen(
-                    stories: _stories,
-                    index: i,
-                  ),
+                  builder: (_) => StoryViewScreen(story: s),
                 ),
               );
+              await StoryService.markViewed(s['id']);
+              _load();
             },
-            child: Container(
-              width: 70,
-              margin: const EdgeInsets.symmetric(horizontal: 6),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
               child: Column(
                 children: [
-                  CircleAvatar(
-                    radius: 28,
-                    backgroundImage: NetworkImage(s['avatar']),
+                  Container(
+                    padding: const EdgeInsets.all(2),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                        color: s['is_viewed'] == true
+                            ? Colors.grey
+                            : Colors.pink,
+                        width: 2,
+                      ),
+                    ),
+                    child: const CircleAvatar(
+                      radius: 30,
+                      backgroundImage:
+                          NetworkImage('https://placehold.co/100x100'),
+                    ),
                   ),
                   const SizedBox(height: 4),
                   Text(
-                    s['username'],
+                    s['username'] ?? '',
                     style: const TextStyle(fontSize: 12),
-                    overflow: TextOverflow.ellipsis,
                   ),
                 ],
               ),
