b31f6c076d5d7bab7bd0026de2e4603cdb39bd73
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-08 21:02:05 +0500

    Update story_service.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/services/story_service.dart
+++ lib/services/story_service.dart
@@ -1,58 +1,30 @@
 import '../core/api.dart';
 import '../core/http_service.dart';
-import '../models/story.dart';
 
 class StoryService {
-  // ================= GET ALL STORIES =================
-  static Future<List<Story>> getStories() async {
+  // ================= GET STORIES =================
+  static Future<List<dynamic>> getStories() async {
     final res = await HttpService.get(Api.storiesEndpoint);
-
-    if (res == null || res is! List) {
-      return [];
-    }
-
-    return res.map<Story>((e) => Story.fromJson(e)).toList();
+    return res is List ? res : [];
   }
 
   // ================= CREATE STORY =================
-  static Future<Story> createStory({
+  static Future<void> createStory({
     required String mediaUrl,
   }) async {
-    final res = await HttpService.post(
+    await HttpService.post(
       Api.storiesEndpoint,
       {
         'media_url': mediaUrl,
       },
     );
-
-    if (res == null) {
-      throw Exception('Create story failed');
-    }
-
-    return Story.fromJson(res);
   }
 
-  // ================= MARK STORY AS VIEWED =================
+  // ================= MARK AS VIEWED =================
   static Future<void> markViewed(int storyId) async {
     await HttpService.post(
       '${Api.storiesEndpoint}/$storyId/view',
       {},
     );
-  }
-
-  // ================= GET MY STORIES =================
-  static Future<List<Story>> getMyStories() async {
-    final res = await HttpService.get('${Api.storiesEndpoint}/me');
-
-    if (res == null || res is! List) {
-      return [];
-    }
-
-    return res.map<Story>((e) => Story.fromJson(e)).toList();
-  }
-
-  // ================= DELETE STORY =================
-  static Future<void> deleteStory(int storyId) async {
-    await HttpService.delete('${Api.storiesEndpoint}/$storyId');
   }
 }
