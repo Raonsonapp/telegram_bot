21118a0c646df0c55b4bf8c5936590fa92cac279
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-08 16:41:22 +0500

    Update reel_service.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/services/reel_service.dart
+++ lib/services/reel_service.dart
@@ -2,28 +2,25 @@ import '../core/api.dart';
 import '../core/http_service.dart';
 
 class ReelService {
+  // GET REELS
   static Future<List<dynamic>> getReels() async {
-    return await HttpService.get(Api.reelsEndpoint);
+    final res = await HttpService.get(Api.reelsEndpoint);
+    return res is List ? res : [];
   }
 
+  // LIKE
   static Future<void> like(int reelId) async {
     await HttpService.post(
-      '${Api.reelsEndpoint}/$reelId/like',
-      {},
+      '${Api.reelsEndpoint}/like',
+      {'reel_id': reelId},
     );
   }
 
+  // UNLIKE
   static Future<void> unlike(int reelId) async {
     await HttpService.post(
-      '${Api.reelsEndpoint}/$reelId/unlike',
-      {},
-    );
-  }
-
-  static Future<void> save(int reelId) async {
-    await HttpService.post(
-      '${Api.reelsEndpoint}/$reelId/save',
-      {},
+      '${Api.reelsEndpoint}/unlike',
+      {'reel_id': reelId},
     );
   }
 }
