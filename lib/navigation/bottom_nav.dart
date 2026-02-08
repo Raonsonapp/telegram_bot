f79c30baaa23d65d66d583c3c7aa7f88821b0ffb
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-08 20:41:25 +0500

    Update bottom_nav.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/navigation/bottom_nav.dart
+++ lib/navigation/bottom_nav.dart
@@ -5,6 +5,7 @@ import '../screens/search/search_screen.dart';
 import '../screens/reels/reels_screen.dart';
 import '../screens/chat/chat_list_screen.dart';
 import '../screens/profile/profile_screen.dart';
+import '../theme/colors.dart';
 
 class BottomNav extends StatefulWidget {
   const BottomNav({super.key});
@@ -14,7 +15,7 @@ class BottomNav extends StatefulWidget {
 }
 
 class _BottomNavState extends State<BottomNav> {
-  int _index = 0;
+  int _currentIndex = 0;
 
   final List<Widget> _screens = const [
     HomeScreen(),
@@ -24,51 +25,63 @@ class _BottomNavState extends State<BottomNav> {
     ProfileScreen(),
   ];
 
-  void _onTap(int i) {
-    if (i == _index) return;
-    setState(() => _index = i);
+  void _onTap(int index) {
+    if (index == _currentIndex) return;
+    setState(() => _currentIndex = index);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: IndexedStack(
-        index: _index,
+        index: _currentIndex,
         children: _screens,
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _index,
+      bottomNavigationBar: _bottomBar(),
+    );
+  }
+
+  // ================= BOTTOM BAR =================
+  Widget _bottomBar() {
+    return Container(
+      decoration: const BoxDecoration(
+        color: AppColors.black,
+        border: Border(
+          top: BorderSide(color: AppColors.divider, width: 0.3),
+        ),
+      ),
+      child: BottomNavigationBar(
+        currentIndex: _currentIndex,
         onTap: _onTap,
+        backgroundColor: AppColors.black,
         type: BottomNavigationBarType.fixed,
-        backgroundColor: Colors.black,
-        selectedItemColor: Colors.white,
-        unselectedItemColor: Colors.grey,
+        selectedItemColor: AppColors.white,
+        unselectedItemColor: AppColors.grey,
         showSelectedLabels: false,
         showUnselectedLabels: false,
         items: const [
           BottomNavigationBarItem(
-            icon: Icon(Icons.home_outlined),
-            activeIcon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined, size: 28),
+            activeIcon: Icon(Icons.home, size: 28),
             label: 'Home',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            activeIcon: Icon(Icons.search),
+            icon: Icon(Icons.search, size: 28),
             label: 'Search',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.play_circle_outline),
-            activeIcon: Icon(Icons.play_circle),
+            icon: Icon(Icons.play_circle_outline, size: 30),
+            activeIcon: Icon(Icons.play_circle, size: 30),
             label: 'Reels',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.chat_bubble_outline),
-            activeIcon: Icon(Icons.chat_bubble),
+            icon: Icon(Icons.chat_bubble_outline, size: 26),
+            activeIcon: Icon(Icons.chat_bubble, size: 26),
             label: 'Chat',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.person_outline),
-            activeIcon: Icon(Icons.person),
+            icon: Icon(Icons.person_outline, size: 28),
+            activeIcon: Icon(Icons.person, size: 28),
             label: 'Profile',
           ),
         ],
