a101e2808999f9f013cde880b5fe69945443e2fc
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-08 16:26:17 +0500

    Update search_service.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/services/search_service.dart
+++ lib/services/search_service.dart
@@ -2,8 +2,7 @@ import '../core/api.dart';
 import '../core/http_service.dart';
 
 class SearchService {
-  static Future<List<dynamic>> search(String query) async {
-    if (query.isEmpty) return [];
+  static Future<Map<String, dynamic>> search(String query) async {
     return await HttpService.get(
       '${Api.searchEndpoint}?q=$query',
     );
