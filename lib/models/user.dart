3eba65f6186b037b93975684083c72bc7183c871
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-08 16:45:33 +0500

    Update user.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,45 +1,21 @@
-class User {
+class UserModel {
   final int id;
   final String username;
-  final String? avatar;
-  final String? bio;
-  final int followersCount;
-  final int followingCount;
-  final bool isVerified;
+  final String avatar;
+  final String bio;
+  int followers;
+  int following;
+  bool isVerified;
+  bool isFollowing;
 
-  User({
+  UserModel({
     required this.id,
     required this.username,
-    this.avatar,
-    this.bio,
-    required this.followersCount,
-    required this.followingCount,
-    required this.isVerified,
+    required this.avatar,
+    required this.bio,
+    this.followers = 0,
+    this.following = 0,
+    this.isVerified = false,
+    this.isFollowing = false,
   });
-
-  // ===== FROM JSON =====
-  factory User.fromJson(Map<String, dynamic> json) {
-    return User(
-      id: json['id'],
-      username: json['username'],
-      avatar: json['avatar'],
-      bio: json['bio'],
-      followersCount: json['followers_count'] ?? 0,
-      followingCount: json['following_count'] ?? 0,
-      isVerified: json['is_verified'] ?? false,
-    );
-  }
-
-  // ===== TO JSON =====
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'username': username,
-      'avatar': avatar,
-      'bio': bio,
-      'followers_count': followersCount,
-      'following_count': followingCount,
-      'is_verified': isVerified,
-    };
-  }
 }
