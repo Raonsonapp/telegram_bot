7b6e754b0f621f5a70b5d61b7c35676fc1fb9d73
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-08 16:41:50 +0500

    Update reels_screen.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/screens/reels/reels_screen.dart
+++ lib/screens/reels/reels_screen.dart
@@ -11,10 +11,8 @@ class ReelsScreen extends StatefulWidget {
 }
 
 class _ReelsScreenState extends State<ReelsScreen> {
-  final PageController _page = PageController();
   List<dynamic> _reels = [];
   bool _loading = true;
-  final Map<int, VideoPlayerController> _controllers = {};
 
   @override
   void initState() {
@@ -28,156 +26,22 @@ class _ReelsScreenState extends State<ReelsScreen> {
       _reels = data;
       _loading = false;
     });
-    _initController(0);
-  }
-
-  void _initController(int index) {
-    if (_controllers.containsKey(index)) return;
-
-    final c = VideoPlayerController.network(_reels[index]['video']);
-    _controllers[index] = c;
-    c.initialize().then((_) {
-      c.setLooping(true);
-      c.play();
-      setState(() {});
-    });
-  }
-
-  @override
-  void dispose() {
-    for (var c in _controllers.values) {
-      c.dispose();
-    }
-    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     if (_loading) {
-      return const Center(child: CircularProgressIndicator());
+      return const Scaffold(
+        body: Center(child: CircularProgressIndicator()),
+      );
     }
 
-    return PageView.builder(
-      controller: _page,
-      scrollDirection: Axis.vertical,
-      itemCount: _reels.length,
-      onPageChanged: (i) {
-        _controllers.forEach((k, v) => v.pause());
-        _initController(i);
-        _controllers[i]?.play();
-      },
-      itemBuilder: (_, i) => _item(_reels[i], i),
-    );
-  }
-
-  Widget _item(dynamic reel, int index) {
-    final c = _controllers[index];
-
-    return Stack(
-      children: [
-        if (c != null && c.value.isInitialized)
-          SizedBox.expand(
-            child: FittedBox(
-              fit: BoxFit.cover,
-              child: SizedBox(
-                width: c.value.size.width,
-                height: c.value.size.height,
-                child: VideoPlayer(c),
-              ),
-            ),
-          ),
-
-        // RIGHT ACTIONS
-        Positioned(
-          right: 12,
-          bottom: 120,
-          child: Column(
-            children: [
-              _icon(
-                Icons.favorite,
-                reel['liked'] ? Colors.red : Colors.white,
-                () async {
-                  reel['liked']
-                      ? await ReelService.unlike(reel['id'])
-                      : await ReelService.like(reel['id']);
-                  setState(() => reel['liked'] = !reel['liked']);
-                },
-                label: reel['likes'].toString(),
-              ),
-              _icon(
-                Icons.mode_comment,
-                Colors.white,
-                () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (_) => CommentsScreen(postId: reel['id']),
-                    ),
-                  );
-                },
-                label: reel['comments'].toString(),
-              ),
-              _icon(
-                Icons.send,
-                Colors.white,
-                () {},
-              ),
-              _icon(
-                Icons.bookmark,
-                Colors.white,
-                () async {
-                  await ReelService.save(reel['id']);
-                },
-              ),
-            ],
-          ),
-        ),
-
-        // BOTTOM USER
-        Positioned(
-          left: 12,
-          bottom: 40,
-          right: 80,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                '@${reel['username']}',
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
-                ),
-              ),
-              const SizedBox(height: 6),
-              Text(
-                reel['caption'] ?? '',
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-              ),
-            ],
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget _icon(
-    IconData icon,
-    Color color,
-    VoidCallback onTap, {
-    String? label,
-  }) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 16),
-      child: Column(
-        children: [
-          IconButton(
-            icon: Icon(icon, color: color, size: 30),
-            onPressed: onTap,
-          ),
-          if (label != null)
-            Text(label, style: const TextStyle(fontSize: 12)),
-        ],
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: PageView.builder(
+        scrollDirection: Axis.vertical,
+        itemCount: _reels.length,
+        itemBuilder: (_, i) => ReelItem(reel: _reels[i]),
       ),
     );
   }
