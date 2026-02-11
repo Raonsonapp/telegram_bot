0e643777d8d4799bf4f3a6a43adcd6b1fdfac55c
Raonson <ehsonmahmadmurodov@gmail.com>
2026-02-11 22:22:02 +0500

    Update main.dart
    Signed-off-by: Raonson <ehsonmahmadmurodov@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,8 @@
-import 'package:flutter/material.dart';
-import 'app/app.dart';
-import 'core/session_manager.dart';
-import 'core/token_storage.dart';
-import 'core/network_checker.dart';
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // ============================
-  // INIT CORE SERVICES
-  // ============================
-  await TokenStorage.init();
-  await SessionManager.init();
-  await NetworkChecker.init();
-
-  // ============================
-  // GLOBAL ERROR HANDLER
-  // ============================
-  FlutterError.onError = (FlutterErrorDetails details) {
-    FlutterError.presentError(details);
-    debugPrint('🔥 Flutter Error: ${details.exception}');
-  };
-
+  // Нест кардани TokenStorage.init() ва AppConfig.init(), чун онҳо статикӣ ҳастанд
+  // Танҳо сервисҳоеро, ки воқеан ба Init ниёз доранд, нигоҳ дор
+  
   runApp(const RaonsonAppRoot());
-}
-
-///
-/// ROOT WIDGET
-///
-class RaonsonAppRoot extends StatelessWidget {
-  const RaonsonAppRoot({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const RaonsonApp();
-  }
 }
