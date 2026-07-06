6a32e59984e1564a503d4e0b47e180480367180e
Hyperion <ehsonmahmadmurodov@gmail.com>
2026-07-06 21:52:53 +0500

    Update anime.go
    Signed-off-by: Hyperion <ehsonmahmadmurodov@gmail.com>

--- backend/keyboard/anime.go
+++ backend/keyboard/anime.go
@@ -5,9 +5,9 @@ import (
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 
-	"anime-bot/api"
-	"anime-bot/models"
-	"anime-bot/utils"
+	"anime-bot/backend/api"
+	"anime-bot/backend/models"
+	"anime-bot/backend/utils"
 )
 
 // SearchResultsKeyboard рӯйхати натиҷаҳои ҷустуҷӯро ба тугмаҳо табдил медиҳад
