d54387b5a6e08fc515417d2e9a27be4f617a7497
Hyperion <ehsonmahmadmurodov@gmail.com>
2026-07-06 15:27:06 +0500

    Delete lib directory
    Signed-off-by: Hyperion <ehsonmahmadmurodov@gmail.com>

--- lib/main.dart
+++
@@ -1,8 +0,0 @@
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  // Нест кардани TokenStorage.init() ва AppConfig.init(), чун онҳо статикӣ ҳастанд
-  // Танҳо сервисҳоеро, ки воқеан ба Init ниёз доранд, нигоҳ дор
-  
-  runApp(const RaonsonAppRoot());
-}
